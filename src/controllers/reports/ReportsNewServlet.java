3eecc2531230600cd98658ce40e947041a331147
yoko.tanaka <laxmi7lady-laxmi@yahoo.co.jp>
2019-07-12 12:38:37 +0900

    Add Report new

---
+++ src/controllers/reports/ReportsNewServlet.java
@@ -0,0 +1,46 @@
+package controllers.reports;
+
+import java.io.IOException;
+import java.sql.Date;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Report;
+
+/**
+ * Servlet implementation class ReportsNewServlet
+ */
+@WebServlet("/reports/new")
+public class ReportsNewServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ReportsNewServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setAttribute("_token", request.getSession().getId());
+
+        Report r = new Report();
+        r.setReport_date(new Date(System.currentTimeMillis()));
+        request.setAttribute("report", r);
+// 日報の日時は、事前に本日の日付を取得して格納しています。
+// 今日の日報を新規で登録する際、既に今日の日付が入力欄に入っていた方が利用者にとって便利だからです。
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/reports/new.jsp");
+        rd.forward(request, response);
+    }
+
+}
